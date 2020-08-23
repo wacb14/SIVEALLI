7756938a4d0267cf35ca840086826f65634123ef
Ced <robertoced15@gmail.com>
2020-08-22 19:20:59 -0500

    clase cliente

--- Codigo C#/SIVEALLI/SIVEALLI/FormClientes.Designer.cs
+++ Codigo C#/SIVEALLI/SIVEALLI/FormClientes.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(354, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "plin plin plon";
+            // 
+            // FormClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormClientes";
+            this.Controls.Add(this.label1);
+            this.Name = "FormClientes";
+            this.Text = "Clientes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
