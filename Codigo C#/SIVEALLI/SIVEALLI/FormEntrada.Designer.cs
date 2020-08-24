3351dc9c108d9d66061c433306b2e13de83a7b44
Marcelo Choque <mfchn007@gmail.com>
2020-08-24 18:37:29 -0500

    cambios interfaz

--- Codigo C#/SIVEALLI/SIVEALLI/FormEntrada.Designer.cs
+++ Codigo C#/SIVEALLI/SIVEALLI/FormEntrada.Designer.cs
@@ -54,7 +54,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 57);
+            this.label1.Location = new System.Drawing.Point(19, 36);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 19);
@@ -63,16 +63,16 @@
             // 
             // textBoxCodigoEntrada
             // 
-            this.textBoxCodigoEntrada.Location = new System.Drawing.Point(143, 54);
-            this.textBoxCodigoEntrada.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxCodigoEntrada.Location = new System.Drawing.Point(155, 33);
+            this.textBoxCodigoEntrada.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxCodigoEntrada.Name = "textBoxCodigoEntrada";
-            this.textBoxCodigoEntrada.Size = new System.Drawing.Size(149, 26);
+            this.textBoxCodigoEntrada.Size = new System.Drawing.Size(188, 26);
             this.textBoxCodigoEntrada.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 91);
+            this.label2.Location = new System.Drawing.Point(19, 70);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(121, 19);
@@ -82,16 +82,16 @@
             // textBoxUsuario
             // 
             this.textBoxUsuario.Enabled = false;
-            this.textBoxUsuario.Location = new System.Drawing.Point(143, 122);
+            this.textBoxUsuario.Location = new System.Drawing.Point(155, 101);
             this.textBoxUsuario.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxUsuario.Name = "textBoxUsuario";
-            this.textBoxUsuario.Size = new System.Drawing.Size(149, 26);
+            this.textBoxUsuario.Size = new System.Drawing.Size(188, 26);
             this.textBoxUsuario.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 125);
+            this.label3.Location = new System.Drawing.Point(19, 104);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(105, 19);
@@ -100,7 +100,7 @@
             // 
             // dateTimePickerEntrada
             // 
-            this.dateTimePickerEntrada.Location = new System.Drawing.Point(792, 10);
+            this.dateTimePickerEntrada.Location = new System.Drawing.Point(792, 12);
             this.dateTimePickerEntrada.Name = "dateTimePickerEntrada";
             this.dateTimePickerEntrada.Size = new System.Drawing.Size(266, 26);
             this.dateTimePickerEntrada.TabIndex = 6;
@@ -108,14 +108,14 @@
             // comboBoxCodigoProveedor
             // 
             this.comboBoxCodigoProveedor.FormattingEnabled = true;
-            this.comboBoxCodigoProveedor.Location = new System.Drawing.Point(143, 88);
+            this.comboBoxCodigoProveedor.Location = new System.Drawing.Point(155, 67);
             this.comboBoxCodigoProveedor.Name = "comboBoxCodigoProveedor";
-            this.comboBoxCodigoProveedor.Size = new System.Drawing.Size(149, 27);
+            this.comboBoxCodigoProveedor.Size = new System.Drawing.Size(188, 27);
             this.comboBoxCodigoProveedor.TabIndex = 7;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(149, 46);
+            this.textBox1.Location = new System.Drawing.Point(147, 43);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(177, 26);
@@ -124,7 +124,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 49);
+            this.label4.Location = new System.Drawing.Point(19, 46);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 19);
@@ -134,7 +134,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(372, 49);
+            this.label5.Location = new System.Drawing.Point(371, 46);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(64, 19);
@@ -143,9 +143,9 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(443, 46);
+            this.numericUpDown1.Location = new System.Drawing.Point(442, 42);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.Size = new System.Drawing.Size(72, 26);
             this.numericUpDown1.TabIndex = 11;
             // 
             // buttonAniadir
@@ -154,7 +154,7 @@
             this.buttonAniadir.FlatAppearance.BorderSize = 0;
             this.buttonAniadir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAniadir.Image = ((System.Drawing.Image)(resources.GetObject("buttonAniadir.Image")));
-            this.buttonAniadir.Location = new System.Drawing.Point(570, 24);
+            this.buttonAniadir.Location = new System.Drawing.Point(521, 20);
             this.buttonAniadir.Margin = new System.Windows.Forms.Padding(4);
             this.buttonAniadir.Name = "buttonAniadir";
             this.buttonAniadir.Size = new System.Drawing.Size(68, 66);
@@ -167,7 +167,7 @@
             this.buttonNuevaEntrada.FlatAppearance.BorderSize = 0;
             this.buttonNuevaEntrada.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonNuevaEntrada.Image = ((System.Drawing.Image)(resources.GetObject("buttonNuevaEntrada.Image")));
-            this.buttonNuevaEntrada.Location = new System.Drawing.Point(300, 33);
+            this.buttonNuevaEntrada.Location = new System.Drawing.Point(360, 12);
             this.buttonNuevaEntrada.Margin = new System.Windows.Forms.Padding(4);
             this.buttonNuevaEntrada.Name = "buttonNuevaEntrada";
             this.buttonNuevaEntrada.Size = new System.Drawing.Size(68, 66);
@@ -185,7 +185,7 @@
             this.groupBoxDatosEntrada.Controls.Add(this.comboBoxCodigoProveedor);
             this.groupBoxDatosEntrada.Location = new System.Drawing.Point(12, 12);
             this.groupBoxDatosEntrada.Name = "groupBoxDatosEntrada";
-            this.groupBoxDatosEntrada.Size = new System.Drawing.Size(372, 179);
+            this.groupBoxDatosEntrada.Size = new System.Drawing.Size(435, 152);
             this.groupBoxDatosEntrada.TabIndex = 35;
             this.groupBoxDatosEntrada.TabStop = false;
             this.groupBoxDatosEntrada.Text = "Datos de documento de entrada";
@@ -197,9 +197,9 @@
             this.groupBoxEntradaProducto.Controls.Add(this.buttonAniadir);
             this.groupBoxEntradaProducto.Controls.Add(this.label5);
             this.groupBoxEntradaProducto.Controls.Add(this.numericUpDown1);
-            this.groupBoxEntradaProducto.Location = new System.Drawing.Point(404, 91);
+            this.groupBoxEntradaProducto.Location = new System.Drawing.Point(453, 64);
             this.groupBoxEntradaProducto.Name = "groupBoxEntradaProducto";
-            this.groupBoxEntradaProducto.Size = new System.Drawing.Size(654, 100);
+            this.groupBoxEntradaProducto.Size = new System.Drawing.Size(605, 100);
             this.groupBoxEntradaProducto.TabIndex = 36;
             this.groupBoxEntradaProducto.TabStop = false;
             this.groupBoxEntradaProducto.Text = "Datos de producto";
@@ -225,7 +225,7 @@
             this.Controls.Add(this.groupBoxDatosEntrada);
             this.Controls.Add(this.dateTimePickerEntrada);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormEntrada";
             this.Text = "FormEntrada";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
