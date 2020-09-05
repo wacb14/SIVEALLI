3183cc5fbc60c4851b1906e752b2b9009f58344c
mfchn <69769377+mfchn@users.noreply.github.com>
2020-09-05 10:55:03 -0500

    Create CambiosBd.sql

---
+++ Base de Datos/CambiosBd.sql
@@ -0,0 +1,42 @@
+--Cambio ventas (igv, descuento)
+--Adicion tabla TNegocio
+
+delete from TVentaDetalle
+delete from TVenta
+
+alter table TVenta
+add Descuento bit not null;
+go
+
+alter table TVenta
+add IGV float not null;
+go
+
+
+create table TNegocio
+(
+	Nombre varchar(25),
+	FechaInicio date,
+	Duegno varchar(40),
+	RUC varchar(11),
+	Telefono varchar(11),
+	Correo varchar(30),
+	Direccion varchar(35),
+	IGV float,
+	MontoDescuento float,
+)
+-----------------------------------------------------------
+-----------------------------------------------------------
+--Modificacion tabla negocio
+
+alter table TNegocio
+add IdModificacion int identity(0,1) not null;
+go
+
+alter table TNegocio
+drop column FechaInicio;
+go
+
+alter table TNegocio
+add Fecha date not null;
+go
