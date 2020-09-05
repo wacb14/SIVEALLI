7d8e55345d5bf436dcbd742a745a3751e50f86dd
mfchn <69769377+mfchn@users.noreply.github.com>
2020-09-05 12:07:09 -0500

    Update CambiosBd.sql

--- Base de Datos/CambiosBd.sql
+++ Base de Datos/CambiosBd.sql
@@ -1,6 +1,3 @@
---Cambio ventas (igv, descuento)
---Adicion tabla TNegocio
-
 delete from TVentaDetalle
 delete from TVenta
 
@@ -12,35 +9,21 @@ alter table TVenta
 add IGV float not null;
 go
 
+--drop table TNegocio
 
 create table TNegocio
 (
+	IdModificacion int identity(0,1) not null,
 	Nombre varchar(25),
-	FechaInicio date,
 	Duegno varchar(40),
 	RUC varchar(11),
 	Telefono varchar(11),
 	Correo varchar(30),
 	Direccion varchar(35),
 	IGV float,
-	MontoDescuento float,
+	MontoSuperarDescuento float,
+	PorcentajeDescuento float,
+	Fecha date,
+
+	primary key (IdModificacion)
 )
------------------------------------------------------------
------------------------------------------------------------
---Modificacion tabla negocio
-
-alter table TNegocio
-add IdModificacion int identity(0,1) not null;
-go
-
-alter table TNegocio
-drop column FechaInicio;
-go
-
-alter table TNegocio
-add Fecha date not null;
-go
-
-alter table TNegocio
-add primary key (IdModificacion);
-go
